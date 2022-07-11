b07c9de83c4991806a3849d34952dc2de62de712
Vishalddd <99655224+Vishalddd@users.noreply.github.com>
2022-07-11 11:49:21 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cloudkitchenweb'`, () => {
+  it(`should have as title 'Vishal_deshmukh'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cloudkitchenweb');
+    expect(app.title).toEqual('Vishal_deshmukh');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cloudkitchenweb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Vishal_deshmukh app is running!');
   });
 });
